99b8959760bbd118177f3d6dda12a28710acedfd
neb <56274697+neb-dev@users.noreply.github.com>
2020-11-30 22:09:27 -0700

    Add files via upload

--- project3/app.js
+++ project3/app.js
@@ -1,6 +1,26 @@
 const app = new Vue({
   el: '#events',
   data: {
-    counter: 0
-  }
+    counter: 0,
+    name: '',
+    enteredName: '',
+  },
+  methods: {
+    add(value) {
+      this.counter += value;
+    },
+    subtract(value) {
+      this.counter -= value;
+    },
+    nameHandler(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    enteredNameHandler() {
+      this.enteredName = this.name;
+    },
+    submit(event) {
+      alert('Sent!');
+      // event.preventDefault();
+    },
+  },
 });
