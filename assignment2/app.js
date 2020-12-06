1244272507a7680bcbe1187ad1de3c445f188641
neb <56274697+neb-dev@users.noreply.github.com>
2020-12-05 23:43:45 -0700

    Add files via upload

---
+++ assignment2/app.js
@@ -0,0 +1,29 @@
+const app = new Vue({
+    el: '#assignment',
+    data: {
+        result: 0,
+    },
+    methods: {
+        add(val) {
+            this.result += val;
+        },
+    },
+    computed: {
+        messageHandler() {
+            if(this.result < 37) {
+                return 'Not there yet';
+            } else if(this.result === 37) {
+                return 37;
+            } else {
+                return 'Too Much!';
+            }
+        },
+    },
+    watch: {
+        messageHandler(val) {
+            if(val !== 'Not there yet') {
+                setTimeout(() => {this.result = 0}, 5000);
+            }
+        }
+    },
+});
