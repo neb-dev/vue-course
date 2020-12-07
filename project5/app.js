1eaf7eb774cd9b9a30134b76c5bd50a36145f31d
neb <56274697+neb-dev@users.noreply.github.com>
2020-12-06 20:06:55 -0700

    Add files via upload

---
+++ project5/app.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+    el: '#styling',
+    data: {
+        boxA: false,
+        boxB: false,
+        boxC: false,
+    },
+    methods: {
+        selectedBox(letter) {
+            if(letter === 'A') {
+                this.boxA = !this.boxA;
+            } else if(letter === 'B') {
+                this.boxB = !this.boxB;
+            } else if(letter === 'C') {
+                this.boxC = !this.boxC;
+            }
+        },
+    },
+});
