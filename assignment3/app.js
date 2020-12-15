c9f4b59633016839bda5d1d866f4bda5e1bef0ec
neb <56274697+neb-dev@users.noreply.github.com>
2020-12-14 17:41:51 -0700

    Add files via upload

--- assignment3/app.js
+++ assignment3/app.js
@@ -1,32 +1,32 @@
 const app = new Vue({
-    el: '#assignment',
-    data: {
-        inputClass: '',
-        isVisible: true,
-        isHidden: false,
-        inputColor: '',
+  el: '#assignment',
+  data: {
+    inputClass: '',
+    isVisible: true,
+    isHidden: false,
+    inputColor: '',
+  },
+  methods: {
+    toggleVisibility() {
+      this.isVisible = !this.isVisible;
+      this.isHidden = !this.isHidden;
     },
-    methods: {
-        toggleVisibility() {
-            this.isVisible = !this.isVisible;
-            this.isHidden = !this.isHidden;
-        }
+  },
+  computed: {
+    classSelector() {
+      if (this.inputClass === 'user1') {
+        return 'user1';
+      } else if (this.inputClass === 'user2') {
+        return 'user2';
+      }
     },
-    computed: {
-        classSelector() {
-            if(this.inputClass === 'user1') {
-                return 'user1';
-            } else if(this.inputClass === 'user2') {
-                return 'user2';
-            }
-        },
-        colorSelector() {
-            if(this.inputColor === '') {
-                return;
-            }
-            return this.inputColor;
-
-
-        }
+    paraClasses() {
+      return {
+        user1: this.inputClass === 'user1',
+        user2: this.inputClass === 'user2',
+        visible: this.isVisible,
+        hidden: this.isHidden,
+      };
     },
+  },
 });
