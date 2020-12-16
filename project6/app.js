fe823f3cac82c6d6eec806f749e3c305551285e3
neb <56274697+neb-dev@users.noreply.github.com>
2020-12-15 21:56:53 -0700

    Add files via upload

--- project6/app.js
+++ project6/app.js
@@ -4,10 +4,23 @@ const app = new Vue({
   data: {
     inputGoal: '',
     goals: [],
+    // fam: [
+    //   {
+    //     name: 'Jessica',
+    //     age: 30,
+    //   },
+    //   {
+    //     name: 'Bailey',
+    //     age: 1.5
+    //   }
+    // ]
   },
   methods: {
     addGoal() {
       this.goals.push(this.inputGoal);
+    },
+    deleteGoal(ind) {
+      this.goals.splice(ind, 1);
     },
   },
 });
