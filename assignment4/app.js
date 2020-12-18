510f099015f77596ae6f463fb6ac3f43a50a4b42
neb <56274697+neb-dev@users.noreply.github.com>
2020-12-17 21:28:21 -0700

    Add files via upload

--- assignment4/app.js
+++ assignment4/app.js
@@ -1,12 +1,27 @@
 const app = Vue.createApp({
-    el: '#assignment',
-    data: {
-        tasks: [],
-        inputTask: ''
+    data() {
+        return {
+            tasks: [],
+            inputTask: '',
+            isVis: true,
+        };
     },
     methods: {
         addTask() {
             this.tasks.push(this.inputTask);
+        },
+        toggleTasks() {
+            this.isVis = !this.isVis;
+        },
+    },
+    computed: {
+        toggleCaption() {
+            if(this.isVis) {
+                return 'Hide tasks';
+            } else if (!this.isVis) {
+                return 'Show tasks';
+            }
         }
-    }
+    },
 });
+app.mount('#assignment');
