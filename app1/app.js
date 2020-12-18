7513fa510d6be5bda5d4b5d606856193554d8be5
neb <56274697+neb-dev@users.noreply.github.com>
2020-12-17 22:13:48 -0700

    Add files via upload

--- app1/app.js
+++ app1/app.js
@@ -1,8 +1,26 @@
-var game = {
+function attackDmg(min, max) {
+  // calculate a random number between min and max
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const game = {
   data() {
-    return {};
+    return {
+      playerHp: 100,
+      monsterHp: 100,
+    };
   },
-  methods: {},
+  methods: {
+    playerAttack() {
+      const pVal = attackDmg(2, 10);
+      this.monsterHp -= pVal;
+      this.monsterAttack();
+    },
+    monsterAttack() {
+      const mVal = attackDmg(5, 20);
+      this.playerHp -= mVal;
+    },
+  },
   computed: {},
   watch: {},
 };
