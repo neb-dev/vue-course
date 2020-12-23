64692660c81871e84c86158ea23de7143b9547ca
neb <56274697+neb-dev@users.noreply.github.com>
2020-12-23 00:06:01 -0700

    Add files via upload

--- project7/app.js
+++ project7/app.js
@@ -16,3 +16,25 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+// Proxy
+const data = {
+  message: 'Hello!',
+  nextMessage: 'Hello! World!',
+};
+
+const handler = {
+  set(target, key, value) {
+    // target = set data property, key = get data property, value = the value\
+    if (key === 'message') {
+      target.nextMessage = value + ' World.';
+    }
+    target.message = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Hello,';
+
+console.log(proxy.nextMessage);
