bcc305cb05353a452a76dabf578a3c7ac0e3193a
neb <56274697+neb-dev@users.noreply.github.com>
2021-01-01 10:49:01 -0700

    Add files via upload

--- project8/app.js
+++ project8/app.js
@@ -1,6 +1,50 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      friends: [
+        {
+          id: 'jessica',
+          name: 'Jessica Lastname',
+          phone: '1231234456',
+          email: 'jess@email.com',
+        },
+        {
+          id: 'josh',
+          name: 'Joshua Lastname',
+          phone: '1231234456',
+          email: 'josh@email.com',
+        },
+      ],
+    };
+  },
+});
+
+app.component('f-component', {
+  template: `
+  <li>
+    <h2>{{ f.name }}</h2>
+    <button @click="toggleVis">Show Details</button>
+    <ul v-if="isVis">
+      <li><strong>Phone:</strong> {{ f.phone }}</li>
+      <li><strong>Email:</strong> {{ f.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      isVis: false,
+      f: {
+        id: 'josh',
+        name: 'Joshua Lastname',
+        phone: '1231234456',
+        email: 'josh@email.com',
+      },
+    };
+  },
+  methods: {
+    toggleVis() {
+      this.isVis = !this.isVis;
+    },
   },
 });
 
