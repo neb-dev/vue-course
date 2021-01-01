14926fb9382db700df09ad3f28968508ac2923b8
neb <56274697+neb-dev@users.noreply.github.com>
2021-01-01 14:56:59 -0700

    Add files via upload

---
+++ project9/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<section>
+<h2>Friends List</h2>
+<ul>
+    <li></li>
+</ul>
+</section>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'jessica',
+          name: 'Jessica Lastname',
+          phone: '1231234456',
+          email: 'jess@email.com',
+        },
+        {
+          id: 'josh',
+          name: 'Joshua Lastname',
+          phone: '1231234456',
+          email: 'josh@email.com',
+        },
+      ],
+    };
+  },
+};
+</script>
