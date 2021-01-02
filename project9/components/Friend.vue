be3e7f09df99e0de66c8d9d27d6fff08ddfe00ec
neb <56274697+neb-dev@users.noreply.github.com>
2021-01-02 10:30:16 -0700

    Add files via upload

--- project9/components/Friend.vue
+++ project9/components/Friend.vue
@@ -1,5 +1,31 @@
-<template> </template>
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleVis">{{ isVis ? 'Hide' : 'Show' }} Details</button>
+  </li>
+  <ul v-if="isVis">
+    <li><strong>Phone:</strong>{{ friend.phone }}</li>
+    <li><strong>Email:</strong>{{ friend.email }}</li>
+  </ul>
+</template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isVis: false,
+      friend: {
+        id: 'josh',
+        name: 'Joshua Lastname',
+        phone: '1231234456',
+        email: 'josh@email.com',
+      },
+    };
+  },
+  methods: {
+    toggleVis() {
+      this.isVis = !this.isVis;
+    },
+  },
+};
 </script>
