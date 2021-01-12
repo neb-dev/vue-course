1b7e0e37abf4a76a3dc34faf5351c2779a35030a
neb <56274697+neb-dev@users.noreply.github.com>
2021-01-12 10:53:52 -0700

    Add files via upload

--- assignment5/components/ActiveUser.vue
+++ assignment5/components/ActiveUser.vue
@@ -1,10 +1,9 @@
 <template>
-<div v-if="user">
-<h2>Username: {{ user.name }}</h2>
-  <h3>Age: {{ user.age }}</h3>
-</div>
-<h1 v-else>No User</h1>
-  
+  <div v-if="user.name && user.age">
+    <h2>Username: {{ user.name }}</h2>
+    <h3>Age: {{ user.age }}</h3>
+  </div>
+  <h1 v-else>No User</h1>
 </template>
 
 <script>
@@ -14,6 +13,6 @@ export default {
       type: Object,
       required: true,
     },
-  }
+  },
 };
 </script>
