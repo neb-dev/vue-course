28e4e2fc8ab6efaad9ea214fe4df702e30af64bc
neb <56274697+neb-dev@users.noreply.github.com>
2021-01-14 13:00:29 -0700

    Add files via upload

--- project10/src/components/KnowledgeGrid.vue
+++ project10/src/components/KnowledgeGrid.vue
@@ -6,14 +6,12 @@
       :id="topic.id"
       :topic-name="topic.title"
       :description="topic.description"
-      @select-topic="$emit('select-topic', $event)"
     ></knowledge-element>
   </ul>
 </template>
 
 <script>
 export default {
-  inject: ['topics'], // topics can be passed down to child component
-  emits: ['select-topic']
+  inject: ['topics'],
 };
 </script>
