ec20bdbd05043c7dacd46826914758abf6d189b7
neb <56274697+neb-dev@users.noreply.github.com>
2021-02-04 21:13:17 -0700

    Add files via upload

--- app2/components/UI/BaseButton.vue
+++ app2/components/UI/BaseButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="type" :class="mode">
+    <button :class="mode"> <!-- :type="type" -->
         <slot></slot>
     </button>
 </template>
@@ -7,10 +7,10 @@
 <script>
 export default {
     props: {
-        type: {
-            type: String,
-            required: true,
-        },
+        // type: {
+        //     type: String,
+        //     required: true,
+        // },
         mode: {
             type: String,
             required: true,
