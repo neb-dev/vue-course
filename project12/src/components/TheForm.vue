1022b0f9b0cbe883f766c6e62cf5609fb3a1c7b4
neb <56274697+neb-dev@users.noreply.github.com>
2021-02-11 21:10:24 -0700

    Add files via upload

--- project12/src/components/TheForm.vue
+++ project12/src/components/TheForm.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referral">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -58,12 +58,14 @@ export default {
     return {
       userName: '',
       userAge: null,
+      referral: 'wom',
     };
   },
   methods: {
     submitForm() {
       this.userName = '';
       this.userAge = null;
+      this.referral = 'wom';
     }
   }
 };
